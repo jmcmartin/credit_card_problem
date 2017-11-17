1654bbcc6b5352ebf193e86bc2e03325c89a37c4
Josh McMartin <mcmartinjd@s.dcsdk12.org>
2017-11-17 08:24:02 -0700

    complete number 0

--- credit_card.py
+++ credit_card.py
@@ -1,21 +1,33 @@
-# http://code.activestate.com/recipes/172845-python-luhn-checksum-for-credit-card-validation/
-# https://stackoverflow.com/questions/40392929/check-a-valid-credit-card-number-using-python
-
 # Instructions: https://www.braceyourself.io/public/files/apcsp/credit_card.txt
 
 import math
 
 #Number 0
 
-def count_digits(card_number):
-	return len(card_number)
+def count_card_digits(card_number):
+	length = 0
+	while card_number > 0:
+		card_number = card_number / 10
+		length = length + 1
 
-print count_digits("5424180123456789")
+	return length
 
+
+print count_card_digits(5424180123456789)
+
+def check_card_number(card_number):
+	digits = digits
+	while card_number > 0:
+		card_number = card_number / 10
+		digits = card_number % 10
+
+	return digits
+
+print check_card_number(5424180123456789)
 
 #Number 1
 	#Method 1
-def check_card_number(card_number):
+"""def check_card_number(card_number):
 	sum_of_digits = 0
 	for i in card_number:
 		if i % 2 == 0:
@@ -37,15 +49,7 @@ def check_card_number(card_number):
 		return "This card number is not valid: ", card_number
 
 
-print check_card_number("5424180123456789")
+print check_card_number("5424180123456789")"""
 
-
-	#Method 2
-def check_card_number(card_number):
-	for digit in card_number:
-		digit = card_number % 10
-
-
-print check_card_number(5424180123456789)
 
 #Number 2
